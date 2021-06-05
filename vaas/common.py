31f42b3854d68b48c4d7e3d5527cc86af15f00de
lizh <lizhenghui_dlu@sina.com>
2021-06-05 12:03:15 +0800

    init code

---
+++ vaas/common.py
@@ -0,0 +1,8 @@
+# Common Libs, include generate signature, data encryption and decryption
+
+class Common(object):
+	"""docstring for ClassName"""
+	def __init__(self, arg):
+		super(ClassName, self).__init__()
+		self.arg = arg
+
---
+++ vaas/video.py
@@ -0,0 +1,2 @@
+# video info service
+#
