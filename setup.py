c8c98d3967d1d86faae341f484c4980ecd33cb4d
wangmq <wangmq@yilan.tv>
2021-06-15 17:56:45 +0800

    s

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="vaas-api-sdk",
+    version="1.0.0",
+    description="vaas-sdk",
+    author="yilan",
+    url="https://github.com/yilanyun/vaas-sdk-python.git",
+    packages=find_packages()
+)
