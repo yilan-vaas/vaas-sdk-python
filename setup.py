4a4a01053d398c93083ea15c824ea83cd92da601
wangmq <wangmq@yilan.tv>
2022-02-24 15:10:26 +0800

    chage tag

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="vaassdk",
-    version="1.0.7",
+    version="1.0.8",
     description="vaassdk",
     author="yilan",
     url="https://github.com/yilanyun/vaas-sdk-python.git",
